1c2c1d3c466b2b6a248ae9c128586215c5945065
Abhishek-Sharma0 <112159863+Abhishek-Sharma0@users.noreply.github.com>
2023-09-28 13:11:31 +0530

    Add files via upload

---
+++ src/Components/WorkCardData.js
@@ -0,0 +1,32 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Stopwatch",
+        text: "Defined with three buttons play, pause, reset.",
+        // Github - https://github.com/Abhishek-Sharma0/Stopwatch 
+        view: "https://abhishek-sharma0.github.io/Stopwatch/",
+    },
+
+    {
+        imgsrc: pro2,
+        title: "Superhero Page",
+        text: "This project is Super Hero Hunter App in which we use superheroAPI() to fetch the records and details of all the superheroes along with some additional features. It is built using HTML, CSS, Vanilla Javascript. ",
+        // Github - https://github.com/Abhishek-Sharma0/Superhero-Page-App-
+        view: "https://abhishek-sharma0.github.io/Superhero-Page-App-/",
+    },
+
+    {
+        imgsrc: pro3,
+        title: "Alarm Clock",
+        text: "It alows user to set alarm at a specific time and it rings for a minute. ",
+        // Github - https://github.com/Abhishek-Sharma0/Alarm_Clock 
+        view: "https://abhishek-sharma0.github.io/Alarm_Clock/",
+    }
+]
+
+export default ProjectCardData;
